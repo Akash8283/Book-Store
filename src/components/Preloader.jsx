f40c64396477d914e301cd04ece44fe7178e4b47
akash <akashks304@gmail.com>
2025-11-25 10:31:56 +0530

    project setup completed

---
+++ src/components/Preloader.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Preloader() {
+  return (
+    <div className='h-screen flex justify-center items-center '><img className='w-3/12' src="https://cdn-icons-gif.flaticon.com/15557/15557327.gif" alt="" /></div>
+  )
+}
+
+export default Preloader
