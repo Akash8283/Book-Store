82914f84131c2eaa94511c54814f003dccd31ab9
akash <akashks304@gmail.com>
2025-11-25 12:58:25 +0530

    header completed, home partial

--- src/users/components/Header.jsx
+++ src/users/components/Header.jsx
@@ -1,8 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaBars, FaInstagram, FaUser, FaYoutube } from 'react-icons/fa'
+import { FaXTwitter } from 'react-icons/fa6'
+import { Link } from 'react-router-dom'
 
 function Header() {
+  const [listStaus,setListStatus] = useState(false)
+
+  const menuVBtnClick =()=>{
+    setListStatus(!listStaus)
+  }
+
   return (
-    <div>Header</div>
+    <>
+    {/* header upper part - title & login */}
+    <div className='grid grid-cols-3 p-2'>
+      {/* logo with titile */}
+      <div className="flex items-center">
+        <img width={'60px'} height={'60px'} src="https://png.pngtree.com/png-vector/20240515/ourmid/pngtree-open-book-logo-png-image_12467719.png" alt="logo" />
+        <h1 className="text-2xl font-bold ms-1 md:hidden">BOOKSTORE</h1>
+      </div>
+      {/* title */}
+      <div className='md:flex justify-center items-center hidden'>
+        <h1 className='text-3xl font-bold'>BOOKSTORE</h1>
+      </div>
+      {/* login */}
+      <div className='md:flex justify-end items-center hidden'>
+        <FaInstagram/>
+        <FaYoutube className='mx-2'/>
+        <FaXTwitter/>
+        <Link to={'/login'} className='ms-4 border rounded py-1 px-2 hover:bg-black hover:text-white flex items-center'><FaUser className='me-1'/> Login</Link>
+      </div>
+    </div>
+    {/* header lower part - links & menu + login btn */}
+    <nav className='w-full p-2 bg-black text-white md:flex justify-center items-center'>
+      {/* div - menu bar & login btn in mobile screen */}
+      <div className='flex justify-between items-center text-2xl md:hidden'>
+        {/* menu bar btn */}
+        <button onClick={menuVBtnClick}><FaBars/></button>
+        {/* login link */}
+         <Link to={'/login'} className='ms-4 border rounded py-1 px-2 hover:bg-white hover:text-black flex items-center'><FaUser className='me-1'/> Login</Link>
+      </div>
+      {/* ul links */}
+      <ul className={listStaus?"flex flex-col":"md:flex justify-center hidden"}>
+        <li className='md:mx-4 mt-3 md:mt-0'><Link to={'/'} >HOME</Link></li>
+        <li className='md:mx-4 my-3 md:my-0'><Link to={'/books'} >BOOKS</Link></li>
+        <li className='md:mx-4 mb-3 md:mb-0'><Link to={'/contact'} >CONTACT</Link></li>
+      </ul>
+    </nav>
+    </>
   )
 }
 
--- src/users/pages/Home.jsx
+++ src/users/pages/Home.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
-
+import Header from '../components/Header'
+import Footer from '../../components/Footer'
+import { FaSearch } from 'react-icons/fa'
 function Home() {
   return (
-    <div>Home</div>
+    <>
+      <Header/>
+       <div>
+         {/* landing part - search */}
+         <div style={{height:'500px'}} className='flex justify-center items-center flex-col bg-[url(/books2.jpg)] bg-cover bg-center text-white'>
+         <div style={{height:'500px',backgroundColor:'rgba(0,0,0,0.6)'}} className='flex justify-center items-center flex-col w-full'>
+          <h1 className='text-5xl font-bold mb-2'>Wonderful gifts</h1>
+          <p>Gift your family and friends a book</p>
+          {/* seacrh */}
+          <div className='mt-9 flex items-center'>
+            <input type="text" className='rounded-3xl bg-white text-2xl text-black w-100 placeholder-gray-500 p-2' placeholder='Search Books Here'/>
+            <button className='text-gray-500' style={{marginLeft:"-40px"}}><FaSearch/></button>
+          </div>
+         </div>
+         </div>
+         {/* new arrival */}
+         <section className='md:px-40 p-5 my-5 flex flex-col justify-center items-center'>
+           <h1 className="text-3xl font-bold">NEW ARRIVALS</h1>
+           <h2 className='text-2xl'>Explore Our Latest Collections</h2>
+           {/* books row & col */}
+         </section>
+         {/* author */}
+         {/* testimony */}
+       </div>
+      <Footer/>
+    </>
   )
 }
 
