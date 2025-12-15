cc45ec0b669ca7b44a4059b0e4e319cd7b5bb3a3
akash <akashks304@gmail.com>
2025-12-15 20:04:02 +0530

    sellbook completed

--- src/users/components/SellBook.jsx
+++ src/users/components/SellBook.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
 import { FaPlus } from 'react-icons/fa';
+import { ToastContainer, toast } from 'react-toastify';
+import { addBookAPI } from '../../services/allAPI';
 
 function SellBook() {
     const [bookDetails,setBookDetails] = useState({
-        title:"",author:"",pages:"",price:"",discountPrice:"",imageURL:"",abstract:"",language:"",publisher:"",isbn:"",category:"",uploadImage:[]
+        title:"",author:"",pages:"",price:"",discountPrice:"",imageURL:"",abstract:"",language:"",publisher:"",isbn:"",category:"",uploadImages:[]
     })
     const [preview,setPreview] = useState("")
     const [previewList,setPreviewList] = useState([])
@@ -14,9 +16,9 @@ function SellBook() {
     //   get file which upload
     console.log(e.target.files[0]);
     // add files to state
-    const imgFileArray = bookDetails.uploadImage
+    const imgFileArray = bookDetails.uploadImages
     imgFileArray.push(e.target.files[0])
-    setBookDetails({...bookDetails,uploadImage:imgFileArray})
+    setBookDetails({...bookDetails,uploadImages:imgFileArray})
     // convert file to url
     const url = URL.createObjectURL(e.target.files[0])
     console.log(url);
@@ -24,6 +26,53 @@ function SellBook() {
     const bookImagesArray = previewList
     bookImagesArray.push(url)
     setPreviewList(bookImagesArray)
+    }
+
+    const handleUploadBook = async()=>{
+        const {title,author, pages,price,discountPrice,imageURL,abstract,language,publisher,isbn,category,uploadImages} = bookDetails
+            if (!title || !author || !pages || !price ||!discountPrice || !imageURL || !abstract || !language || !publisher || !isbn || !category || uploadImages.length==0) {
+                toast.info("Please fill the form completely")
+            }
+            else{
+                // api call - addbookapi
+                const token = sessionStorage.getItem("token")
+                if (token) {
+                    const reqHeader = {
+                        "Authorization" : `Bearer ${token}`
+                    }
+                    const reqBody = new FormData()
+                    for(let key in bookDetails){
+                        if (key != "uploadImages") {
+                            reqBody.append(key,bookDetails[key])
+                        }
+                        else{
+                            bookDetails.uploadImages.forEach(imgFile=>{
+                                reqBody.append("uploadImages",imgFile)
+                            })
+                        }
+                    }
+                    const result = await addBookAPI(reqBody,reqHeader)
+                    console.log(result);
+                    if (result.status == 200) {
+                        toast.success("Book Added successfully")
+                    }
+                    else if(result.status == 401){
+                        toast.warning(result.response.data)
+                    }
+                    else{
+                        toast.error("Something went wrong")
+                    }
+                    resetUploadBookForm()
+                }
+            }
+    }
+
+    const resetUploadBookForm = ()=>{
+        setBookDetails({
+            title:"",author:"",pages:"",price:"",discountPrice:"",imageURL:"",abstract:"",language:"",publisher:"",isbn:"",category:"",uploadImages:[]
+        })
+        setPreview("")
+        setPreviewList([])
     }
 
   return (
@@ -96,9 +145,14 @@ function SellBook() {
             </div>
         </div>
         <div className="flex justify-end">
-            <button className='bg-gray-500 text-white p-2 rounded me-5 hover:bg-white hover:text-gray-800'>RESET</button>
-            <button className='bg-blue-600 text-white p-2 rounded  hover:bg-white hover:text-gray-800'>ADD BOOK</button>
+            <button onClick={resetUploadBookForm} className='bg-gray-500 text-white p-2 rounded me-5 hover:bg-white hover:text-gray-800'>RESET</button>
+            <button onClick={handleUploadBook} className='bg-blue-600 text-white p-2 rounded  hover:bg-white hover:text-gray-800'>ADD BOOK</button>
         </div>
+        <ToastContainer
+                position="top-center"
+                autoClose={2000}
+                theme="dark"
+         />
     </div>
   )
 }
